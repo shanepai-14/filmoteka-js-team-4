aab0b38c17fce4bd4b886e6c52ec77274e7e8e19
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-05-17 01:18:07 +0800

    add

--- src/js/api.js
+++ src/js/api.js
@@ -3,9 +3,9 @@ import { BASE_URL, API_KEY } from "./utils";
 
 import axios from "axios";
 
-async  function  getMoviesPopular() {
+export async  function  getMoviesPopular(page) {
     try{
-        const response  =  await axios.get(`${BASE_URL}3/movie/popular?api_key=${API_KEY}`, {
+        const response  =  await axios.get(`${BASE_URL}3/movie/popular?api_key=${API_KEY}&page=${page}`, {
             headers: { 
                 accept: 'application/json',
                 Authorization: `Bearer ${API_KEY}` },
@@ -13,11 +13,54 @@ async  function  getMoviesPopular() {
     
         console.log(response);
             return response;
-    }catch(e){
-        console.log(e);
-    }
+    }catch(error){
+        console.log(error);
+        return [];
     }
 
-    console.log(BASE_URL);
-    getMoviesPopular
 
+    }
+         
+export async function searchMovies(query,page){
+    try{
+
+        const response  =  await axios.get(`${BASE_URL}3/search/movie?api_key=${API_KEY}&query=${query}&page=${page}`, {
+            headers: { 
+                accept: 'application/json',
+                Authorization: `Bearer ${API_KEY}` },
+          });
+     ;
+    
+        console.log(response);
+            return response;
+    }catch(error) {  
+        console.log(error);
+        return [];
+    }
+}
+
+export async function MovieController(query, page = 1){
+    if(query !== '' && query !== undefined){
+        const result = await searchMovies(query,page);
+        return result;
+    }
+    const result = await getMoviesPopular(page);
+     return result;
+
+}
+
+export async function getMovieDetails(id){
+    try {
+        const response = await axios.get(
+          `${BASE_URL}movie/${id}?api_key=${API_KEY}`
+        );
+  
+        console.log(response.data);
+  
+        return response.data;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+
+}
