6d9a6a02bb28e78ee4027563db1ff657a3e593da
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-05-29 18:41:18 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './js/modal.js';
 import './js/header.js';
 import './js/upcoming.js';
-// import './js/header_library.js';
+
 
 import {
   MovieController,
@@ -16,6 +16,9 @@ const searchInput = document.getElementById('input-textbox-home');
 const movieContainer = document.querySelector('.movie-container');
 const listLoader = document.querySelector('.loader');
 const listPagination = document.querySelector('.is-hiddenPagination');
+const libraryList = document.querySelector('.library');
+const libraryListQ = document.querySelector('.libraryQ');
+const libraryListW = document.querySelector('.libraryW');
 let currentPage = 1;
 
 async function movieList(page = 1) {
@@ -112,6 +115,112 @@ searchForm.addEventListener('submit', async (e) => {
 
   }
 });
+
+function displayResultLibrary(dataResult) {
+  listPagination.classList.add('is-hiddenPagination');
+  let finalResult = dataResult.map(result => {
+    let year = result.release_date.split('-');
+    return movieCard(
+      result.id,
+      result.poster_path,
+      result.title,
+      result.genre_ids,
+      year
+    );
+  });
+  movieContainer.insertAdjacentHTML('beforeend', finalResult.join(''));
+}
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', function (e) {
+    if (e.target && e.target.classList.contains('libraryBtn')) {
+      const buttonText = e.target.textContent;
+      const movieId = e.target.closest('.modalBtnContainer');
+      let filmotekaWatchMovies = '';
+      const movieData = {
+        id: movieId.dataset.id,
+      };
+
+      if (buttonText === 'Add to watched') {
+        filmotekaWatchMovies =
+          JSON.parse(localStorage.getItem('filmotekaWatchMovies')) || [];
+      }
+      if (buttonText === 'Add to queue') {
+        filmotekaWatchMovies =
+          JSON.parse(localStorage.getItem('filmotekaQueueMovies')) || [];
+      }
+
+      const isMovieWatched = filmotekaWatchMovies.some(
+        filmotekaWatchMovies => filmotekaWatchMovies.id === movieData.id
+      );
+
+      if (!isMovieWatched) {
+        if (buttonText === 'Add to watched') {
+          filmotekaWatchMovies.push(movieData);
+          localStorage.setItem(
+            'filmotekaWatchMovies',
+            JSON.stringify(filmotekaWatchMovies)
+          );
+        }
+        if (buttonText === 'Add to queue') {
+          filmotekaWatchMovies.push(movieData);
+          localStorage.setItem(
+            'filmotekaQueueMovies',
+            JSON.stringify(filmotekaWatchMovies)
+          );
+        }
+
+        Notiflix.Notify.success('Successfully added to the List!');
+      } else {
+        Notiflix.Notify.info('This Movie is already added.');
+      }
+    }
+  });
+});
+async function watchedListData(x) {
+  let watchedMoviesShow = '';
+  if (x === 0) {
+    watchedMoviesShow =
+      JSON.parse(localStorage.getItem('filmotekaWatchMovies')) || [];
+  }
+  if (x === 1) {
+    watchedMoviesShow =
+      JSON.parse(localStorage.getItem('filmotekaQueueMovies')) || [];
+  }
+  if (watchedMoviesShow.length === 0) {
+    Notiflix.Notify.info('No watch movies to display.');
+    return;
+  }
+
+  listLoader.classList.remove('is-hidden');
+
+  movieContainer.innerHTML = '';
+  for (const movie of watchedMoviesShow) {
+
+    try {
+      const searchResult = await getMovieDetails(movie.id);
+      displayResultLibrary([searchResult]);
+
+
+
+    } catch (error) {
+      console.error('Error fetching list:', error);
+
+    }
+  }
+  listLoader.classList.add('is-hidden');
+}
+libraryList.addEventListener('click', () => {
+  watchedListData(0);
+});
+
+libraryListQ.addEventListener('click', () => {
+  watchedListData(1);
+});
+
+libraryListW.addEventListener('click', () => {
+  watchedListData(0);
+});
+
 window.addEventListener("load", (event) => {
   movieList();
 });
